8f6ae5cba5ee36a9158e1d008bb75427b4cc185a
Vishal Kaushik <vishalkaushik@Vishals-MacBook-Pro.local>
2021-11-02 15:01:18 +0530

    Implemented Get task by id

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,5 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { CreateTaskDto } from './dto/create-task.dto';
 //import { title } from 'process';
 import { Task } from './tasks.model';
 import { TasksService } from './tasks.service';
@@ -7,16 +8,21 @@ import { TasksService } from './tasks.service';
 export class TasksController {
   constructor(private tasksService: TasksService) {}
 
+  // To get all the tasks created
   @Get()
   getAllTasks(): Task[] {
     return this.tasksService.getAllTasks();
   }
 
+  // To get a specific task by id
+  @Get('/:id')
+  getTaskById(@Param('id') id: string): Task {
+    return this.tasksService.getTaskById(id);
+  }
+
+  // To create a new task
   @Post()
-  createTask(
-    @Body('title') title: string,
-    @Body('description') description: string,
-  ): Task {
-    return this.tasksService.createTask(title, description);
+  createTask(@Body() createTaskDto: CreateTaskDto): Task {
+    return this.tasksService.createTask(createTaskDto);
   }
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,16 +1,25 @@
 import { Injectable } from '@nestjs/common';
 import { Task, TaskStatus } from './tasks.model';
 import { v4 as uuid } from 'uuid';
+import { CreateTaskDto } from './dto/create-task.dto';
 
 @Injectable()
 export class TasksService {
   private tasks: Task[] = [];
 
+  // Returns all the tasks created
   getAllTasks(): Task[] {
     return this.tasks;
   }
 
-  createTask(title: string, description: string): Task {
+  // Returns a task by id
+  getTaskById(id: string): Task {
+    return this.tasks.find((task) => task.id === id);
+  }
+
+  // Creates a new task task and pushed it to the array
+  createTask(createTaskDto: CreateTaskDto): Task {
+    const { title, description } = createTaskDto;
     const task: Task = {
       id: uuid(),
       title,
